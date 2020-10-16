6b8738bc4dec49dab497e27efaa0aa1e0f674c6d
aglegaspi <lex83nyc@gmail.com>
2020-10-16 02:18:14 -0400

    update site url

--- Sources/AlexandersPortfolio/main.swift
+++ Sources/AlexandersPortfolio/main.swift
@@ -14,7 +14,7 @@ struct AlexandersPortfolio: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://your-website-url.com")!
+    var url = URL(string: "https://aglegaspi.github.io")!
     var name = "Alexander's Portfolio"
     var description = "A portfolio of a Software Engineer"
     var language: Language { .english }
