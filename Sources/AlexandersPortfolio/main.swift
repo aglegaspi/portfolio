89a5529f06fdccc6ce1be3972853ab2098be716e
aglegaspi <lex83nyc@gmail.com>
2020-10-15 21:09:19 -0400

    add extension for wrapper and item list

--- Sources/AlexandersPortfolio/main.swift
+++ Sources/AlexandersPortfolio/main.swift
@@ -21,5 +21,26 @@ struct AlexandersPortfolio: Website {
     var imagePath: Path? { nil }
 }
 
+private extension Node where Context == HTML.BodyContext {
+    static func wrapper(_ nodes: Node...) -> Node {
+        .div(.class("wrapper"), .group(nodes))
+    }
+    
+    static func itemList<T: Website>(for items: [Item<T>], on site: T) -> Node {
+        return .ul(
+            .class("item-list"),
+            .forEach(items) { item in
+                .li(.article(
+                    .h1(.a(
+                        .href(item.path),
+                        .text(item.title)
+                    )),
+                    .p(.text(item.description))
+                )) // li
+            } // forEach
+        ) // ul
+    }
+}
+
 // This will generate your website using the built-in Foundation theme:
 try AlexandersPortfolio().publish(withTheme: .foundation)
